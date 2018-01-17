a3517c61661861ce95e69f4066254aa0c6dd16c5
leoShih <leo201325@gmail.com>
2018-01-17 22:07:08 +0800

    Initial Commit

---
+++ #88 Memorable Places/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  #88 Memorable Places
+//
+//  Created by Leo on 17/01/2018.
+//  Copyright © 2018 Leo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
